ffb92dcbc1a8c1f46f90668a111ea34e07db9b9d
ElipticNight <secondary@aidan-byrne.org.uk>
2021-01-02 16:46:25 +0000

    created example broadcast

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\hello;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/broadcast', function () {
+    broadcast(new hello());
+});
+
