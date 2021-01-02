c45475ff739d86744859feaf7f1ae141fcc2ed32
ElipticNight <secondary@aidan-byrne.org.uk>
2021-01-02 17:44:07 +0000

    added button to trigger event

--- app/Events/hello.php
+++ app/Events/hello.php
@@ -7,10 +7,11 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class hello implements ShouldBroadcast
+class hello implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -27,7 +28,7 @@ class hello implements ShouldBroadcast
     public function broadcastWith()
     {
         return[
-            'hello' => 'there'
+            'Hello World!'
         ];
     }
 
